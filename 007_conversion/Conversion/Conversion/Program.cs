e033d3cea5058729ecca23f62a09e5dabd876d0c
Moser <moserturbo@gmail.com>
2024-05-18 21:53:44 +0200

    Conversions
    Implicit
    Explicit
    Parse/TryParse
    Conversion methods

---
+++ 007_conversion/Conversion/Conversion/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.CodeDom.Compiler;
+
+//You have the following values to convert:
+
+byte a = 10; //Convert this value into "short" type (assign into another short type of variable)
+short ab;
+ab = a;
+
+
+int b = 10; //Convert this value into "short" type (assign into another short type of variable)
+short bc;
+bc = (short)b;
+
+
+string c = "10.34"; //Convert this value into "double" type using Parse  //Also, convert the same value into "decimal" type  using TryParse
+double cc;
+cc = double.Parse(c);
+bool cd = decimal.TryParse(c, out decimal ce);
+
+
+decimal d = 20.3M; //Convert this value into "string" type (assign into another string type of variable)
+string de = System.Convert.ToString(d);
+
+
+
+//And then print all the converted values i.e 10, 10, 10.34, 10.34, 11.56.
+System.Console.Write($"{ab}, {bc}, {cc}, {ce}, {de}.");
+
+System.Console.ReadKey();
