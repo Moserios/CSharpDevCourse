30de9dfabb0e91549abb7219a27c4d8e2edfea46
Moser <moserturbo@gmail.com>
2024-05-31 12:29:30 +0200

    OOP: Properties

---
+++ 004_OOP/OOP Properties/ClassLibrary1/Class1.cs
@@ -0,0 +1,52 @@
+﻿//Create a C# program with a class called DebitCard with a property called "Pin" with validations in the set accessor.
+//The Pin should be exactly 4 digits or 6 digits.
+//If the supplied pin is invalid, it shows error message.
+
+
+//Examples:
+//Pin = "1234"-- > valid
+//Pin = "1234ef"-- > invalid
+//Pin = "123456"-- > valid
+//Pin = "0"-- > invalid
+//Pin = "a4"-- > invalid
+
+using System.Diagnostics.Tracing;
+using System.Net.NetworkInformation;
+
+public class DebitCard
+{
+    private int _pin;
+
+    public string Pin
+    {
+        
+        get { return System.Convert.ToString(_pin); }
+    
+        set 
+        {
+            bool a = int.TryParse(value, out int validPin);
+
+            if (value.Length == 4 || value.Length == 6)
+                if (a is true)
+                {
+                    _pin = validPin;
+                    System.Console.WriteLine($"{value} --> valid");
+                }
+                else
+                {
+                    System.Console.WriteLine($"{value} --> invalid");
+                }
+            else 
+            {
+                System.Console.WriteLine($"{value} --> invalid");
+            }
+        }
+
+    }
+
+    public DebitCard(string pin)
+    {
+        this.Pin = pin;
+    }
+
+}
---
+++ 004_OOP/OOP Properties/OOP Properties/Program.cs
@@ -0,0 +1,25 @@
+﻿//Create a C# program with a class called DebitCard with a property called "Pin" with validations in the set accessor.
+//The Pin should be exactly 4 digits or 6 digits.
+//If the supplied pin is invalid, it shows error message.
+
+
+//Examples:
+//Pin = "1234"-- > valid
+//Pin = "1234ef"-- > invalid
+//Pin = "123456"-- > valid
+//Pin = "0"-- > invalid
+//Pin = "a4"-- > invalid
+
+public class Program
+{
+    public static void Main()
+    {
+        DebitCard card1 = new DebitCard("1234");
+        DebitCard card2 = new DebitCard("1234ef");
+        DebitCard card3 = new DebitCard("123456");
+        DebitCard card4 = new DebitCard("0");
+        DebitCard card5 = new DebitCard("a4");
+        System.Console.ReadKey();
+    }
+}
+
